d08a3fe9b6df1b9667b8e01d44ebd0698076cefb
Manh Phan <manhamsterdam2003@gmail.com>
2026-04-04 18:37:14 +0700

    update

--- JQUERY/1. Import/js/main.js
+++ JQUERY/1. Import/js/main.js
@@ -1,4 +1,3 @@
-// viết 1 câu lệnh đơn giản
-$(document).ready(function () {
-    alert('Bạn đã import file js thành công!');
-})
+version https://git-lfs.github.com/spec/v1
+oid sha256:ae59e9b1c443705021aee9cdbcce042aab65df760dc21adbebd162e0bb914750
+size 125
